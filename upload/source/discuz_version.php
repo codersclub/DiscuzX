9b5834065a41d9f9eef790642335a958428a755b
康盛Discuz! <discuzaddon@tencent.com>
2016-12-15 21:20:54 +0800

    初次创建

---
+++ upload/source/discuz_version.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: discuz_version.php 36281 2016-12-09 08:11:45Z nemohou $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if(!defined('DISCUZ_VERSION')) {
+	define('DISCUZ_VERSION', 'X3.3');
+	define('DISCUZ_RELEASE', '20170101');
+	define('DISCUZ_FIXBUG', '33000000');
+}
+
+?>
