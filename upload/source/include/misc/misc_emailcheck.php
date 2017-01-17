b2dddfcfa486e7fef83117ca4cd96d0ab6d8bf33
SNIPER722 <tom8201@hotmail.com>
2017-01-18 01:12:17 +0800

    邮箱验证漏洞修复

--- upload/source/include/misc/misc_emailcheck.php
+++ upload/source/include/misc/misc_emailcheck.php
@@ -36,7 +36,7 @@ if($uid && isemail($email) && $time > TIMESTAMP - 86400) {
 	} elseif($ucresult == -6) {
 		showmessage('profile_email_duplicate', '', array(), array('return' => true));
 	}
-	if($member['groupid'] == 8) {
+	if($_G['setting']['regverify'] == 1 && $member['groupid'] == 8) {
 		$membergroup = C::t('common_usergroup')->fetch_by_credits($member['credits']);
 		$setarr['groupid'] = $membergroup['groupid'];
 	}
