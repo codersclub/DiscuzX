ffba5153b13efa48e5fdbbb9a77cd5bac6bef685
Discuz! <discuzaddon@tencent.com>
2017-02-08 10:41:52 +0800

    修复 getMulti 方式获取缓存无效的问题

--- upload/source/class/memory/memory_driver_apcu.php
+++ upload/source/class/memory/memory_driver_apcu.php
@@ -7,41 +7,37 @@
  *      $Id: memory_driver_apc.php 27635 2012-02-08 06:38:31Z zhangguosheng $
  */
 if (!defined('IN_DISCUZ')) {
-    exit('Access Denied');
+	exit('Access Denied');
 }
 
 class memory_driver_apcu {
 
-    public function init($config) {
-        
-    }
+	public function init($config) {
+		
+	}
 
-    public function get($key) {
-        return apcu_fetch($key);
-    }
+	public function get($key) {
+		return apcu_fetch($key);
+	}
 
-    public function getMulti($keys) {
-        return apcu_fetch($key);
-    }
+	public function set($key, $value, $ttl = 0) {
+		return apcu_store($key, $value, $ttl);
+	}
 
-    public function set($key, $value, $ttl = 0) {
-        return apcu_store($key, $value, $ttl);
-    }
+	public function rm($key) {
+		return apcu_delete($key);
+	}
 
-    public function rm($key) {
-        return apcu_delete($key);
-    }
+	public function clear() {
+		return apcu_clear_cache('user');
+	}
 
-    public function clear() {
-        return apcu_clear_cache('user');
-    }
+	public function inc($key, $step = 1) {
+		return apcu_inc($key, $step) !== false ? apcu_fetch($key) : false;
+	}
 
-    public function inc($key, $step = 1) {
-        return apcu_inc($key, $step) !== false ? apcu_fetch($key) : false;
-    }
-
-    public function dec($key, $step = 1) {
-        return apcu_dec($key, $step) !== false ? apcu_fetch($key) : false;
-    }
+	public function dec($key, $step = 1) {
+		return apcu_dec($key, $step) !== false ? apcu_fetch($key) : false;
+	}
 
 }
