5f90403eed59f43c3029ec420b582c2a2d8784b7
stackia <jsq2627@gmail.com>
2017-02-12 09:06:29 +0800

    修复 主题查看数不更新问题

--- upload/source/class/table/table_forum_threadaddviews.php
+++ upload/source/class/table/table_forum_threadaddviews.php
@@ -20,7 +20,9 @@ class table_forum_threadaddviews extends discuz_table {
 	}
 
 	public function update_by_tid($tid) {
-		return DB::query('UPDATE %t SET `addviews`=`addviews`+1 WHERE tid=%d', array($this->_table, $tid));
+		$ret = DB::query('UPDATE %t SET `addviews`=`addviews`+1 WHERE tid=%d', array($this->_table, $tid));
+		$this->increase_cache(array($tid), array('addviews' => 1));
+		return $ret;
 	}
 	public function fetch_all_order_by_tid($start = 0, $limit = 0) {
 		return DB::fetch_all('SELECT * FROM %t ORDER BY tid'.DB::limit($start, $limit), array($this->_table), $this->_pk);
