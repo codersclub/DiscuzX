2efb765de0000ddf307616801afaa58f1d94d77f
Comsenz <10433182@qq.com>
2017-04-01 11:26:35 +0800

    Release 20170401

--- upload/source/class/discuz/discuz_application.php
+++ upload/source/class/discuz/discuz_application.php
@@ -378,17 +378,19 @@ class discuz_application extends discuz_base{
 
 	private function _get_client_ip() {
 		$ip = $_SERVER['REMOTE_ADDR'];
-		if (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
-			$ip = $_SERVER['HTTP_CLIENT_IP'];
-		} elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
-			foreach ($matches[0] AS $xip) {
-				if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
-					$ip = $xip;
-					break;
+		if (!$this->config['security']['onlyremoteaddr']) {
+			if (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			} elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) AND preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+				foreach ($matches[0] AS $xip) {
+					if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+						$ip = $xip;
+						break;
+					}
 				}
 			}
 		}
-		return $ip;
+		return $ip == '::1' ? '127.0.0.1' : $ip;
 	}
 
 	private function _init_db() {
--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -13,7 +13,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.3');
-	define('DISCUZ_RELEASE', '20170301');
+	define('DISCUZ_RELEASE', '20170401');
 	define('DISCUZ_FIXBUG', '33000000');
 }
 
