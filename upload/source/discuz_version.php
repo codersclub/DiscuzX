3bb0a0b99e338ace56bf51f90649922748df12f4
Comsenz <10433182@qq.com>
2017-07-10 14:16:15 +0800

    更改版本号

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -12,9 +12,9 @@ if(!defined('IN_DISCUZ')) {
 }
 
 if(!defined('DISCUZ_VERSION')) {
-	define('DISCUZ_VERSION', 'X3.3');
-	define('DISCUZ_RELEASE', '20170401');
-	define('DISCUZ_FIXBUG', '33000000');
+	define('DISCUZ_VERSION', 'X3.4');
+	define('DISCUZ_RELEASE', '20170701');
+	define('DISCUZ_FIXBUG', '34000000');
 }
 
 ?>
