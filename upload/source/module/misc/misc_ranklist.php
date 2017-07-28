89bc5727f1fba3a8579adf090baa2866115c8e2c
Comsenz <10433182@qq.com>
2017-07-28 09:26:55 +0800

    修复 排行的语法严谨性

--- upload/source/module/misc/misc_ranklist.php
+++ upload/source/module/misc/misc_ranklist.php
@@ -427,6 +427,9 @@ function getranklist_member_post($num, $orderby) {
 }
 
 function getranklistdata($type, $view = '', $orderby = 'all') {
+	if (!function_exists('getranklist_'.$type)) {
+	    return array();
+	}
 	global $_G;
 	$cache_time = $_G['setting']['ranklist'][$type]['cache_time'];
 	$cache_num =  $_G['setting']['ranklist'][$type]['show_num'];
@@ -440,6 +443,15 @@ function getranklistdata($type, $view = '', $orderby = 'all') {
 
 	$ranklistvars = array();
 	loadcache('ranklist_'.$type);
+	if(!isset($_G['cache']['ranklist_'.$type]) || !is_array($_G['cache']['ranklist_'.$type])) {
+		$_G['cache']['ranklist_'.$type] = array();
+	}
+	if(!isset($_G['cache']['ranklist_'.$type][$view]) || !is_array($_G['cache']['ranklist_'.$type][$view])) {
+		$_G['cache']['ranklist_'.$type][$view] = array();
+	}
+	if(!isset($_G['cache']['ranklist_'.$type][$view][$orderby]) || !is_array($_G['cache']['ranklist_'.$type][$view][$orderby])) {
+		$_G['cache']['ranklist_'.$type][$view][$orderby] = array();
+	}
 	$ranklistvars = & $_G['cache']['ranklist_'.$type][$view][$orderby];
 
 	if(empty($ranklistvars['lastupdated']) || (TIMESTAMP - $ranklistvars['lastupdated'] > $cache_time)) {
