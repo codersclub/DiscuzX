4887604d808da41df39daced2207b557372faf72
康盛Discuz! <discuzaddon@tencent.com>
2017-10-21 09:20:29 +0800

    修复 完善XSS漏洞的修复

--- upload/source/function/function_core.php
+++ upload/source/function/function_core.php
@@ -210,7 +210,7 @@ function dhtmlspecialchars($string, $flags = null) {
 		if($flags === null) {
 			$string = str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $string);
 			if(strpos($string, '&amp;#') !== false) {
-				$string = preg_replace('/&amp;((#(\d{3,5}|x[a-fA-F0-9]{4}));)/', '&\\1', $string);
+				$string = preg_replace('/&amp;((#(\d{3,5}));)/', '&\\1', $string);
 			}
 		} else {
 			if(PHP_VERSION < '5.4.0') {
