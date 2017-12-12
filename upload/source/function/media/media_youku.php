5785043780214cbac41f7fcd32cee3f9c200d911
他乡启程 <qichengzx@gmail.com>
2017-12-12 14:04:26 +0800

    !136   fix优酷视频URL有多余参数导致解析错误的问题 Merge pull request !136 from 他乡启程/master

--- upload/source/function/media/media_youku.php
+++ upload/source/function/media/media_youku.php
@@ -9,17 +9,18 @@ $checkurl = array('v.youku.com/v_show/');
 function media_youku($url, $width, $height) { 
 	$ctx = stream_context_create(array('http' => array('timeout' => 10)));
 	if(preg_match("/^https?:\/\/v.youku.com\/v_show\/id_([^\/]+)(.html|)/i", $url, $matches)) {
-	    	$flv = 'https://player.youku.com/player.php/sid/'.$matches[1].'/v.swf';
-	    	$iframe = 'https://player.youku.com/embed/'.$matches[1];
-	    	if(!$width && !$height) {
-	    		$api = 'http://v.youku.com/player/getPlayList/VideoIDS/'.$matches[1];
-	    		$str = stripslashes(file_get_contents($api, false, $ctx));
-	    		if(!empty($str) && preg_match("/\"logo\":\"(.+?)\"/i", $str, $image)) {
-	    			$url = substr($image[1], 0, strrpos($image[1], '/')+1);
-	    			$filename = substr($image[1], strrpos($image[1], '/')+2);
-	    			$imgurl = $url.'0'.$filename;
-	    		}
-	    	}
+		$params = explode('.', $matches[1]);
+		$flv = 'https://player.youku.com/player.php/sid/'.$params[0].'/v.swf';
+		$iframe = 'https://player.youku.com/embed/'.$params[0];
+		if(!$width && !$height) {
+			$api = 'http://v.youku.com/player/getPlayList/VideoIDS/'.$params[0];
+			$str = stripslashes(file_get_contents($api, false, $ctx));
+			if(!empty($str) && preg_match("/\"logo\":\"(.+?)\"/i", $str, $image)) {
+				$url = substr($image[1], 0, strrpos($image[1], '/')+1);
+				$filename = substr($image[1], strrpos($image[1], '/')+2);
+				$imgurl = $url.'0'.$filename;
+			}
+		}
 	}
 	return array($flv, $iframe, $url, $imgurl);
 }
