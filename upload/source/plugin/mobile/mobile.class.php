e04046758e34250a82234ab99a4d4126e4db86b9
yaxinliu <yaxinliu@tencent.com>
2019-08-26 15:31:27 +0800

    删除外部指定编码逻辑，防止可能出现的xss漏洞

--- upload/source/plugin/mobile/mobile.class.php
+++ upload/source/plugin/mobile/mobile.class.php
@@ -307,9 +307,6 @@ class base_plugin_mobile {
 		$_G['siteurl'] = preg_replace('/api\/mobile\/$/', '', $_G['siteurl']);
 		$_G['setting']['msgforward'] = '';
 		$_G['setting']['cacheindexlife'] = $_G['setting']['cachethreadlife'] = false;
-		if(!$_G['setting']['mobile']['nomobileurl'] && function_exists('diconv') && !empty($_GET['charset'])) {
-			$_GET = mobile_core::diconv_array($_GET, $_GET['charset'], $_G['charset']);
-		}
 		if($_GET['_auth']) {
 			require_once DISCUZ_ROOT.'./source/plugin/wechat/wsq.class.php';
 			$uid = wsq::decodeauth($_GET['_auth']);
