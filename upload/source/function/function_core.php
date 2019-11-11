a22fd496bbfee429c5b9c04bd7c8046d2f8615b9
oldhu <me@oldhu.com>
2019-11-11 14:30:20 +0800

    !254 修复一个小php7.3兼容问题 * php 7.3 兼容性问题，在7.3下，无法直接用"128M"这样的字符串乘1024。

--- upload/source/function/function_core.php
+++ upload/source/function/function_core.php
@@ -1895,14 +1895,16 @@ function getexpiration() {
 }
 
 function return_bytes($val) {
-    $val = trim($val);
-    $last = strtolower($val{strlen($val)-1});
-    switch($last) {
-        case 'g': $val *= 1024;
-        case 'm': $val *= 1024;
-        case 'k': $val *= 1024;
-    }
-    return $val;
+	$last = strtolower($val{strlen($val)-1});
+	if (!is_numeric($val)) {
+		$val = substr(trim($val), 0, -1);
+	}
+	switch($last) {
+		case 'g': $val *= 1024;
+		case 'm': $val *= 1024;
+		case 'k': $val *= 1024;
+	}
+	return $val;
 }
 
 function iswhitelist($host) {
