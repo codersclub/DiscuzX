476e45391f7d459c02e17945ca5ffb44cff07dbd
HelloStars! <2692284716@qq.com>
2020-08-16 23:29:46 +0800

    !465 update upload/uc_server/admin.php. * update upload/uc_server/admin.php.

--- upload/uc_server/admin.php
+++ upload/uc_server/admin.php
@@ -17,7 +17,7 @@ $mtime = explode(' ', microtime());
 $starttime = $mtime[1] + $mtime[0];
 
 define('IN_UC', TRUE);
-define('UC_ROOT', substr(__FILE__, 0, -9));
+define('UC_ROOT', __DIR__.'/');
 define('UC_API', strtolower((is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/'))));
 define('UC_DATADIR', UC_ROOT.'data/');
 define('UC_DATAURL', UC_API.'/data');
