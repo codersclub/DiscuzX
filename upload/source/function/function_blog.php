2ad8e69cd768d309ecba8a27f211bc42376e7a94
老周部落 <laozhoubuluo@126.com>
2020-12-25 17:35:33 +0800

    !225 修复 日志功能提交链接出错 * 修复 去除Manyou残余代码 * 修复 去除Manyou残余代码 * 修复 日志功能提交链接出错

--- upload/source/function/function_blog.php
+++ upload/source/function/function_blog.php
@@ -72,7 +72,7 @@ function blog_post($POST, $olds=array()) {
 		$POST['message'] = getstr($POST['message'], 0, 0, 0, 0, 1);
 		$POST['message'] = censor($POST['message']);
 		$POST['message'] = preg_replace("/\<div\>\<\/div\>/i", '', $POST['message']);
-		$POST['message'] = preg_replace_callback("/\<a\s+href\=\"([^\>]+?)\"\>/i", 'blog_post_callback_blog_check_url_1', $POST['message']);
+		$POST['message'] = preg_replace_callback("/<a .*?href=\"(.*?)\".*?>/is", 'blog_post_callback_blog_check_url_1', $POST['message']);
 	}
 	$message = $POST['message'];
 	if(censormod($message) || censormod($POST['subject']) || $_G['group']['allowblogmod']) {
--- upload/static/js/home.js
+++ upload/static/js/home.js
@@ -219,27 +219,6 @@ function checkImage(url) {
 	return url.match(re);
 }
 
-function quick_validate(obj) {
-	if($('seccode')) {
-		var code = $('seccode').value;
-		var x = new Ajax();
-		x.get('cp.php?ac=common&op=seccode&code=' + code, function(s){
-			s = trim(s);
-			if(s != 'succeed') {
-				alert(s);
-				$('seccode').focus();
-		   		return false;
-			} else {
-				obj.form.submit();
-				return true;
-			}
-		});
-	} else {
-		obj.form.submit();
-		return true;
-	}
-}
-
 function stopMusic(preID, playerID) {
 	var musicFlash = preID.toString() + '_' + playerID.toString();
 	if($(musicFlash)) {
@@ -345,30 +324,6 @@ function showFlash(host, flashvar, obj, shareid) {
 			obj.style.display = '';
 		};
 	}
-}
-
-function userapp_open() {
-	var x = new Ajax();
-	x.get('home.php?mod=spacecp&ac=common&op=getuserapp&inajax=1', function(s){
-		$('my_userapp').innerHTML = s;
-		$('a_app_more').className = 'fold';
-		$('a_app_more').innerHTML = '收起';
-		$('a_app_more').onclick = function() {
-			userapp_close();
-		};
-	});
-}
-
-function userapp_close() {
-	var x = new Ajax();
-	x.get('home.php?mod=spacecp&ac=common&op=getuserapp&subop=off&inajax=1', function(s){
-		$('my_userapp').innerHTML = s;
-		$('a_app_more').className = 'unfold';
-		$('a_app_more').innerHTML = '展开';
-		$('a_app_more').onclick = function() {
-			userapp_open();
-		};
-	});
 }
 
 function startMarquee(h, speed, delay, sid) {
@@ -779,14 +734,6 @@ function resend_mail(mid) {
 	if(mid) {
 		var obj = $('sendmail_'+ mid +'_li');
 		obj.style.display = "none";
-	}
-}
-
-function userapp_delete(id, result) {
-	if(result) {
-		var ids = explode('_', id);
-		var appid = ids[1];
-		$('space_app_'+appid).style.display = "none";
 	}
 }
 
