242e182f17cdc1df954f562288d47ec52ba76ae0
老周部落 <laozhoubuluo@126.com>
2020-12-27 19:59:15 +0800

    !290 优化 触屏版验证码功能和PC端拉齐、手机端验证码降级功能 * 优化 触屏版验证码功能和PC端拉齐

--- upload/source/module/misc/misc_seccode.php
+++ upload/source/module/misc/misc_seccode.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 $idhash = isset($_GET['idhash']) && preg_match('/^\w+$/', $_GET['idhash']) ? $_GET['idhash'] : '';
 $modid = isset($_GET['modid']) && preg_match('/^[\w:]+$/', $_GET['modid']) ? $_GET['modid'] : '';
 
-if($_GET['action'] == 'update') {
+if($_GET['action'] == 'update' && !defined("IN_MOBILE")) {
 
 	$message = '';
 	$showid = 'seccode_'.$idhash;
@@ -81,6 +81,64 @@ if($('$showid')) {
 }
 EOF;
 
+} elseif($_GET['action'] == 'update' && defined("IN_MOBILE") && constant("IN_MOBILE") == 2) {
+	//移动端和PC端验证码功能拉齐
+	$message = '';
+	$showid = 'seccode_'.$idhash;
+	$rand = random(5, 1);
+	$htmlcode = '';
+	$ani = $_G['setting']['seccodedata']['animator'] ? '_ani' : '';
+	if($_G['setting']['seccodedata']['type'] == 2 || $_G['setting']['seccodedata']['type'] == 3) {
+		$message = '<img onclick="updateseccode(\''.$idhash.'\')" width="'.$_G['setting']['seccodedata']['width'].'" height="'.$_G['setting']['seccodedata']['height'].'" src="misc.php?mod=seccode&mobile='.constant("IN_MOBILE").'&update='.$rand.'&idhash='.$idhash.'" class="seccodeimg" alt="" style="display: inline; visibility: visible;" />';
+	} else {
+		if(!is_numeric($_G['setting']['seccodedata']['type']) && preg_match('/^[\w\d:_]+$/i', $_G['setting']['seccodedata']['type'])) {
+			$etype = explode(':', $_G['setting']['seccodedata']['type']);
+			if(count($etype) > 1) {
+				$codefile = DISCUZ_ROOT.'./source/plugin/'.$etype[0].'/seccode/seccode_'.$etype[1].'.php';
+				$class = $etype[1];
+			} else {
+				$codefile = libfile('seccode/'.$_G['setting']['seccodedata']['type'], 'class');
+				$class = $_G['setting']['seccodedata']['type'];
+			}
+			if(file_exists($codefile)) {
+				@include_once $codefile;
+				$class = 'seccode_'.$class;
+				if(class_exists($class)) {
+					$code = new $class();
+					if(method_exists($code, 'make')) {
+						ob_start();
+						$seccode = $code->make($idhash, $modid);
+						make_seccode($seccode);
+						$message = preg_replace("/\r|\n/", '', ob_get_contents());
+						ob_end_clean();
+					}
+				}
+			}
+		} else {
+			$message = '<img onclick="updateseccode(\''.$idhash.'\')" width="'.$_G['setting']['seccodedata']['width'].'" height="'.$_G['setting']['seccodedata']['height'].'" src="misc.php?mod=seccode&mobile='.constant("IN_MOBILE").'&update='.$rand.'&idhash='.$idhash.'" class="seccodeimg" alt="" style="display: inline; visibility: visible;" />';
+		}
+	}
+	$imemode = $_G['setting']['seccodedata']['type'] != 1 ? 'ime-mode:disabled;' : '';
+	$message = str_replace("'", "\'", $message);
+	$seclang = lang('forum/misc');
+	header("Content-Type: application/javascript");
+echo <<<EOF
+if(document.getElementById('$showid')) {
+	if(!document.getElementById('v$showid')) {
+		var sectpl = seccheck_tpl['$idhash'] != '' ? seccheck_tpl['$idhash'].replace(/<hash>/g, 'code$idhash') : '';
+		var sectplcode = sectpl != '' ? sectpl.split('<sec>') : Array('<br />',': ','<br />','');
+		var string = '<input name="seccodehash" type="hidden" value="$idhash" /><input name="seccodemodid" type="hidden" value="$modid" /><input type="text" class="txt px vm" style="ime-mode:disabled;width:115px;background:white;" autocomplete="off" value="" id="seccodeverify_$idhash" name="seccodeverify" placeholder="$seclang[seccode]" fwin="seccode"><span id="v$showid">$message</span>';
+		evalscript(string);
+		document.getElementById('$showid').innerHTML = string;
+	} else {
+		var string = '$message';
+		evalscript(string);
+		document.getElementById('v$showid').innerHTML = string;
+	}
+	$htmlcode
+}
+EOF;
+
 } elseif($_GET['action'] == 'check') {
 
 	include template('common/header_ajax');
@@ -92,15 +150,15 @@ EOF;
 	$refererhost = parse_url($_SERVER['HTTP_REFERER']);
 	$refererhost['host'] .= !empty($refererhost['port']) ? (':'.$refererhost['port']) : '';
 
-	if($_G['setting']['seccodedata']['type'] < 2 && ($refererhost['host'] != $_SERVER['HTTP_HOST']) || $_G['setting']['seccodedata']['type'] == 2 && !extension_loaded('ming') && $_POST['fromFlash'] != 1 || $_G['setting']['seccodedata']['type'] == 3 && $_GET['fromFlash'] != 1) {
+	if(($_G['setting']['seccodedata']['type'] < 2 && ($refererhost['host'] != $_SERVER['HTTP_HOST'])) || ((defined("IN_MOBILE") && in_array($_G['setting']['seccodedata']['type'], array(2, 3)) && ($refererhost['host'] != $_SERVER['HTTP_HOST'])) && ($_G['setting']['seccodedata']['type'] == 2 && !extension_loaded('ming') && $_POST['fromFlash'] != 1 || $_G['setting']['seccodedata']['type'] == 3 && $_GET['fromFlash'] != 1))) {
+		//当模式为英文=0、中文=1两种验证码时，校验Referer和Host是否为同一站点，不是就退出
+		//当模式为Flash=2、语音=3两种验证码时，要么是手机版且Referer和Host为同一站点（不是就退出，X3.5新增验证码降级功能），
+		//要么是Flash+Ming+FromFlash，要么是语音验证码+FromFlash（不是就退出）
+		//这个逻辑浪费了我二三十分钟，写上注释以免后人入坑
 		exit('Access Denied');
 	}
 
 	if(is_numeric($_G['setting']['seccodedata']['type']) || !preg_match('/^[\w\d:_]+$/i', $_G['setting']['seccodedata']['type'])) {
-
-		if(defined('IN_MOBILE') && in_array($_G['setting']['seccodedata']['type'], array(2, 3))) {
-			exit;
-		}
 
 		$seccode = make_seccode();
 
@@ -114,7 +172,7 @@ EOF;
 
 		$code = new seccode();
 		$code->code = $seccode;
-		$code->type = $_G['setting']['seccodedata']['type'];
+		$code->type = (in_array($_G['setting']['seccodedata']['type'], array(2, 3)) && defined("IN_MOBILE")) ? 0 : $_G['setting']['seccodedata']['type'];
 		$code->width = $_G['setting']['seccodedata']['width'];
 		$code->height = $_G['setting']['seccodedata']['height'];
 		$code->background = $_G['setting']['seccodedata']['background'];
