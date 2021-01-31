efaddce2f1a141c8f1deacacc6809ca164359e21
popcorner <351633+popcorner@user.noreply.gitee.com>
2021-01-31 16:10:58 +0800

    !712 优化 ucenter中的数据库版本获取方式及移除safe mode * 优化 ucenter中的数据库版本获取方式及移除safe mode

--- upload/uc_server/control/admin/frame.php
+++ upload/uc_server/control/admin/frame.php
@@ -59,8 +59,7 @@ class control extends adminbase {
 		}
 
 		$serverinfo = PHP_OS.' / PHP v'.PHP_VERSION;
-		$serverinfo .= @ini_get('safe_mode') ? ' Safe Mode' : NULL;
-		$dbversion = $this->db->result_first("SELECT VERSION()");
+		$dbversion = $this->db->version();
 		$fileupload = @ini_get('file_uploads') ? ini_get('upload_max_filesize') : '<font color="red">'.$lang['no'].'</font>';
 		$dbsize = 0;
 		$tablepre = UC_DBTABLEPRE;
@@ -194,7 +193,7 @@ class control extends adminbase {
 	function _get_uc_envstatus() {
 		$version = constant('UC_SERVER_VERSION');
 		$now_ver_gd = function_exists('gd_info')? gd_info() : false;
-		$now_ver = array('PHP' => constant('PHP_VERSION'), 'MySQL' => $this->db->result_first("SELECT VERSION()"), 'gethostbyname' => function_exists('gethostbyname'), 'file_get_contents' => function_exists('file_get_contents'), 'xml_parser_create' => function_exists('xml_parser_create'),
+		$now_ver = array('PHP' => constant('PHP_VERSION'), 'MySQL' => $this->db->version(), 'gethostbyname' => function_exists('gethostbyname'), 'file_get_contents' => function_exists('file_get_contents'), 'xml_parser_create' => function_exists('xml_parser_create'),
 		'FileSock Function' => (function_exists('fsockopen') || function_exists('pfsockopen') || function_exists('stream_socket_client') || function_exists('curl_init')), 'GD' => ($now_ver_gd ? preg_replace('/[^0-9.]+/', '', $now_ver_gd['GD Version']) : false));
 		$req_ver = array('PHP' => '5.3', 'MySQL' => '5.0', 'filter_var' => true, 'gethostbyname' => true, 'file_get_contents' => true, 'xml_parser_create' => true, 'FileSock Function' => true, 'GD' => '1.0');
 		$sug_ver = array('PHP' => '7.1', 'MySQL' => '5.7', 'filter_var' => true, 'gethostbyname' => true, 'file_get_contents' => true, 'xml_parser_create' => true, 'FileSock Function' => true, 'GD' => '2.0');
