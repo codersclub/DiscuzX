e2f0a0b0946106aafe5c61d00ddb8c25335d3f7a
湖中沉 <341936031@qq.com>
2021-02-02 20:33:39 +0800

    update upload/install/include/install_function.php.

--- upload/install/include/install_function.php
+++ upload/install/include/install_function.php
@@ -74,6 +74,7 @@ function check_db($dbhost, $dbuser, $dbpw, $dbname, $tablepre) {
 		} else {
 			show_msg('database_connect_error', $error, 0);
 		}
+		return false;
 	} else {
 		if($query = (($mysqlmode == 'mysql') ? @mysql_query("SHOW TABLES FROM $dbname") : $link->query("SHOW TABLES FROM $dbname"))) {
 			if(!$query) {
