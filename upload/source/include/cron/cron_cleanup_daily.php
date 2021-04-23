d166aac7e29649d66e609bed72d0ac50559b8370
老周部落 <laozhoubuluo@126.com>
2021-04-23 14:08:57 +0800

    !939 修复 每日数据清理时论坛页面缓存目录下 index.htm 被误删除的问题 * 修复 每日数据清理时论坛页面缓存目录下 index.htm 被误删除的问题

--- upload/source/include/cron/cron_cleanup_daily.php
+++ upload/source/include/cron/cron_cleanup_daily.php
@@ -39,6 +39,7 @@ C::t('forum_tradelog')->expiration_finished(7);
 
 if($_G['setting']['cachethreadon']) {
 	removedir($_G['setting']['cachethreaddir'], TRUE);
+	touch($_G['setting']['cachethreaddir'].'/index.htm');
 }
 removedir($_G['setting']['attachdir'].'image', TRUE);
 @touch($_G['setting']['attachdir'].'image/index.htm');
