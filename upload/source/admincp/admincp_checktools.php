15e8fe77ca7bf0d298bb02862b8b789ac1f7a5ed
popcorner <351633+popcorner@user.noreply.gitee.com>
2021-06-16 13:08:06 +0800

    !1041 修复 未选择任何伪静态规则时查看规则显示出占位符的问题 * 修复 未选择任何伪静态规则时查看规则显示出占位符的问题

--- upload/source/admincp/admincp_checktools.php
+++ upload/source/admincp/admincp_checktools.php
@@ -453,7 +453,7 @@ if($operation == 'filecheck') {
 
 	$rule = array();
 	$rewritedata = rewritedata();
-	$rule['{apache1}'] = $rule['{apache2}'] = $rule['{iis}'] = $rule['{iis7}'] = $rule['{zeus}'] = $rule['{nginx}'] = '';
+	$rule['{apache1}'] = $rule['{apache2}'] = $rule['{iis}'] = $rule['{iis7}'] = $rule['{nginx}'] = $rule['{lighttpd}'] = $rule['{caddy}'] = '';
 	foreach($rewritedata['rulesearch'] as $k => $v) {
 		if(!in_array($k, $_G['setting']['rewritestatus'])) {
 			continue;
