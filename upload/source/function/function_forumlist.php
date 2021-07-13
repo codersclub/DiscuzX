75e1f938f00a64f8cb14e43c1131b63ae6749b6e
湖中沉 <341936031@qq.com>
2021-07-13 07:05:44 +0000

    !1139 新增的get_attach方法支持对视频、音频的解析，以便打造更多列表样式 * 新增的get_attach方法支持对视频、音频的解析，以便打造更多列表样式

--- upload/source/function/function_forumlist.php
+++ upload/source/function/function_forumlist.php
@@ -424,9 +424,10 @@ function threadclasscount($fid, $id = 0, $idtype = '', $count = 0) {
 
 }
 
-function get_attach($list){
+function get_attach($list, $video = false, $audio = false){
 	global $_G;
 	require_once libfile('function/post');
+	require_once libfile('function/discuzcode');
 	$tids = $attach_tids = $attachtableid_array = $threadlist_data = $posttableids = array();
 	foreach($list as $value) {
 		$tids[] = $value['tid'];
@@ -440,6 +441,18 @@ function get_attach($list){
 	foreach ($posttableids as $id) {
 		$theards = C::t('forum_post')->fetch_all_by_tid($id, $tids, true, '', 0, 0, 1, null, null, null);
 		foreach($theards as $value) {
+			if($value['message'] && ($video || $audio)){
+				$value['media'] = '';
+				$value['message'] = preg_replace(array("/\[hide=?\d*\](.*?)\[\/hide\]/is"), array(""), $value['message']);
+				$value['msglower'] = strtolower($value['message']);
+				if(strpos($value['msglower'], '[/media]') !== FALSE && $video) {
+					preg_match("/\[media=([\w,]+)\]\s*([^\[\<\r\n]+?)\s*\[\/media\]/is", $value['message'], $value['video']);
+					$threadlist_data[$value['tid']]['media'] = parsemedia($value['video'][1], $value['video'][2]);
+				}elseif(strpos($value['msglower'], '[/audio]') !== FALSE && $audio) {
+					preg_match("/\[audio(=1)*\]\s*([^\[\<\r\n]+?)\s*\[\/audio\]/is", $value['message'], $value['audio']);
+					$threadlist_data[$value['tid']]['media'] = parseaudio($value['audio'][2], 400);
+				}
+			}
 			$threadlist_data[$value['tid']]['message'] = messagecutstr($value['message'], 90);
 			if(in_array($value['tid'], $attach_tids)) {
 				$attachtableid_array[getattachtableid($value['tid'])][] = $value['pid'];
