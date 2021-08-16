3efd3633e93a1ae65e49647067231e6defefa552
popcorner <351633+popcorner@user.noreply.gitee.com>
2021-08-16 11:58:48 +0000

    !1241 优化 部分不遵守国际标准的常见浏览器的相关特殊处理 * 优化 部分不遵守国际标准的常见浏览器的相关特殊处理

--- upload/source/module/portal/portal_attachment.php
+++ upload/source/module/portal/portal_attachment.php
@@ -62,12 +62,17 @@ if($operation == 'delete') {
 	}
 
 	$filesize = $attach['filesize'];
+	// 遵循RFC 6266国际标准，按照RFC 5987中的规则对文件名进行编码
 	$filenameencode = strtolower(CHARSET) == 'utf-8' ? rawurlencode($attach['filename']) : rawurlencode(diconv($attach['filename'], CHARSET, 'UTF-8'));
+
+	// 连2011年发布的国际标准都没能正确支持的浏览器厂商的黑名单列表
+	// 目前包括：UC，夸克，搜狗，百度
+	$rfc6266blacklist = strexists($_SERVER['HTTP_USER_AGENT'], 'UCBrowser') || strexists($_SERVER['HTTP_USER_AGENT'], 'Quark') || strexists($_SERVER['HTTP_USER_AGENT'], 'SogouM') || strexists($_SERVER['HTTP_USER_AGENT'], 'baidu');
 
 	dheader('Date: '.gmdate('D, d M Y H:i:s', $attach['dateline']).' GMT');
 	dheader('Last-Modified: '.gmdate('D, d M Y H:i:s', $attach['dateline']).' GMT');
 	dheader('Content-Encoding: none');
-	dheader('Content-Disposition: attachment; filename="'.(($attach['filename'] == $filenameencode) ? $attach['filename'].'"' : $filenameencode.'"; filename*=utf-8\'\''.$filenameencode));
+	dheader('Content-Disposition: attachment; filename="'.$filenameencode.'"'.(($attach['filename'] == $filenameencode || $rfc6266blacklist) ? '' : '; filename*=utf-8\'\''.$filenameencode));
 	dheader('Content-Type: '.$attach['filetype']);
 	dheader('Content-Length: '.$filesize);
 
