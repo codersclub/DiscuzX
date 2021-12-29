fc9b4b91339d1bb15944e24dd388bd08f25cf4b9
DiscuzX <153619335@qq.com>
2021-12-29 12:28:11 +0000

    !1359 修复审核帖子时的看帖链接可能跳转错误的BUG * 修复审核帖子时的看帖链接可能跳转错误的BUG

--- upload/source/module/forum/forum_viewthread.php
+++ upload/source/module/forum/forum_viewthread.php
@@ -618,7 +618,7 @@ if(!empty($isdel_post)) {
 	}
 	if($updatedisablepos && !$rushreply) {
 		C::t('forum_threaddisablepos')->insert(array('tid' => $_G['tid']), false, true);
-		dheader('Location:'.$_G['siteurl'].'forum.php?mod=viewthread&tid='.$_G['tid']);
+		dheader('Location:'.$_G['siteurl'].'forum.php?mod=viewthread&tid='.$_G['tid'].($_G['forum_auditstatuson'] ? '&modthreadkey='.$_GET['modthreadkey'] : ''));
 	}
 	$ordertype != 1 ? ksort($postarr) : krsort($postarr);
 }
