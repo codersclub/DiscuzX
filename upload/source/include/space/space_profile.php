88f0f581f08686efec2ae72edd865925c7141c85
brotherand2 <547996854@qq.com>
2022-02-07 12:18:30 +0000

    !1466 修复用户名片一直显示收听TA * 修复用户名片一直显示收听TA

--- upload/source/include/space/space_profile.php
+++ upload/source/include/space/space_profile.php
@@ -192,6 +192,9 @@ if(!$_G['privacy']) {
 		include_once template("home/space_profile");
 	} else {
 		$_GET['do'] = 'card';
+		if ( helper_access::check_module('follow') ) {
+			$follow = C::t('home_follow')->fetch_by_uid_followuid($_G['uid'], $space['uid']);//是否收听对方
+		}        
 		include_once template("home/space_card");
 	}
 }
