1fe2dbe403dd2967db0357eeb6d68345b2dab22e
popcorner <351633+popcorner@user.noreply.gitee.com>
2022-07-02 01:37:25 +0000

    !1635 优化 管理后台登录页报错文案 * 优化 管理后台登录页报错文案

--- upload/source/language/lang_admincp_login.php
+++ upload/source/language/lang_admincp_login.php
@@ -33,11 +33,11 @@ $lang = array
 	'login_tips' => 'Discuz! 是 <a href="http://cloud.tencent.com" target="_blank">腾讯云</a>推出的以社区为基础的专业建站平台，帮助网站实现一站式服务。',
 	'login_nosecques' => '您还没有使用安全登录，请在个人中心设置您的安全提问后，再访问管理中心。您可以 <a href="forum.php?mod=memcp&action=profile&typeid=1" target="_blank">点击这里</a> 进入安全提问的设置。',
 
-	'login_cp_guest' => '<b>由于您尚未登录,本次请求已经被拒绝.</b><br><br>请 <a href="member.php?mod=logging&action=login">登录</a> 后再试',
-	'login_cplock' => '您的管理面板已经锁定! <br>请在<b> {ltime} </b>秒以后重新访问管理中心',
-	'login_user_lock' => '由于您的登录密码错误次数过多,本次登录请求已经被拒绝. 请 15 分钟后重新尝试',
-	'login_cp_noaccess' => '<b>管理中心(或此项操作)尚未对您开放</b><br><br>您的此次操作已经记录, 请勿非法尝试',
-	'noaccess' => '后台管理权限(或此项操作)尚未对您开放, 请联系站点管理员',
+	'login_cp_guest' => '<b>由于您尚未登录，本次请求已经被拒绝。</b><br><br>请 <a href="member.php?mod=logging&action=login">登录</a> 后再试。<br><br>站长需要强制登录时，修改 config/config_global.php 可关闭此功能。',
+	'login_cplock' => '您的管理面板已经锁定！<br>请在<b> {ltime} </b>秒以后重新访问管理中心。',
+	'login_user_lock' => '由于您的登录密码错误次数过多，本次登录请求已经被拒绝。请 15 分钟后重新尝试。',
+	'login_cp_noaccess' => '<b>管理中心(或此项操作)尚未对您开放</b><br><br>您的此次操作已经记录，请勿非法尝试',
+	'noaccess' => '后台管理权限(或此项操作)尚未对您开放，请联系站点管理员',
 
 
 );
