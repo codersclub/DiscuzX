6159cbc88f7a5017ddb8b06a9d84483622be1c3d
DiscuzX <153619335@qq.com>
2022-08-09 03:00:12 +0000

    !1670 修复GBK版CC攻击防御的提示信息乱码的问题 * 修复GBK版CC攻击防御的提示信息乱码的问题

--- upload/source/include/misc/misc_security.php
+++ upload/source/include/misc/misc_security.php
@@ -87,6 +87,8 @@ function securitymessage($subject, $message, $reload = TRUE, $form = FALSE) {
 		echo '<div id="attackevasive_1" class="popupmenu_option"><b style="font-size: 16px">'.$subject.'</b><br /><br />'.$message.'</div>';
 		security_ajaxshowfooter();
 	} else {
+		$charset = getglobal('config/output/charset');
+		@header('Content-Type: text/html; charset='.$charset);
 		echo '<html>';
 		echo '<head>';
 		echo '<title>'.$subject.'</title>';
@@ -134,7 +136,7 @@ function security_ajaxshowheader() {
 	@header("Expires: -1");
 	@header("Cache-Control: no-store, private, post-check=0, pre-check=0, max-age=0", FALSE);
 	@header("Pragma: no-cache");
-	header("Content-type: application/xml");
+	@header("Content-type: application/xml; charset=".$charset);
 	echo "<?xml version=\"1.0\" encoding=\"".$charset."\"?>\n<root><![CDATA[";
 }
 
