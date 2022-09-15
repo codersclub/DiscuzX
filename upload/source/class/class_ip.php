d9cf9fbb94653556f93fec5218cc7c4f9400bb04
popcorner <351633+popcorner@user.noreply.gitee.com>
2022-09-15 09:04:37 +0000

    !1741 修复 CIDR校验在特定情况下出现的问题 * 修复 CIDR校验在特定情况下出现的问题

--- upload/source/class/class_ip.php
+++ upload/source/class/class_ip.php
@@ -63,8 +63,12 @@ class ip {
 			if($mask <= 0) {
 				return FALSE;
 			}
+			$newmask = intval($mask);
 			$newip = self::to_ip($newip);
 			if (!self::validate_ip($newip)) {
+				return FALSE;
+			}
+			if($newmask > 128 || ($newmask > 32 && strpos($newip, ':') === FALSE)) {
 				return FALSE;
 			}
 			$new_str = $newip . "/" . $mask;
