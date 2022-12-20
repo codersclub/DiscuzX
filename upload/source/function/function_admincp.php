c9a53581ead76705ff97b2a8f1bc36136940ed86
老周部落 <laozhoubuluo@126.com>
2022-12-20 16:43:55 +0000

    !1836 修复 累计重要安全性更新(X3.5 适配包) * 修复 累计重要安全性更新(X3.5 适配包)

--- upload/source/function/function_admincp.php
+++ upload/source/function/function_admincp.php
@@ -1330,13 +1330,12 @@ function set_pluginsetting($pluginvars) {
 }
 
 function checkformulaperm($formula) {
-	$formula = preg_replace('/(\{([\d\.\-]+?)\})/', "'\\1'", $formula);
-	$formula = str_replace(array('and', 'or'), array('&&', '||'), $formula);
+	$formula = preg_replace('/(\{([0-9a-fA-F\.\-\:\/]+?)\})/', "'\\1'", $formula);
 	return checkformulasyntax(
 		$formula,
-		array('+', '-', '*', '/', '(', ')', '<', '=', '>', '!', '&', '|', ' '),
+		array('+', '-', '*', '/', '<', '<=', '==', '>=', '>', '!=', 'and', 'or'),
 		array('regdate', 'regday', 'regip', 'lastip', 'buyercredit', 'sellercredit', 'digestposts', 'posts', 'threads', 'oltime', 'extcredits[1-8]', 'field[\d]+'),
-		'\'\{[\d\.\-]+\}\''
+		'\'\{[0-9a-fA-F\.\-\:\/]+\}\''
 	);
 }
 
