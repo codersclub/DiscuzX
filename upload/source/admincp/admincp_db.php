19e82969f2260f8847d9b275381c49056ff723c3
DiscuzX <153619335@qq.com>
2023-03-06 01:02:36 +0000

    !2029 修复个别情况下“删除服务器上的备份数据”链接参数错误的问题 * 修复个别情况下“删除服务器上的备份数据”链接参数错误的问题

--- upload/source/admincp/admincp_db.php
+++ upload/source/admincp/admincp_db.php
@@ -285,7 +285,7 @@ if($operation == 'export') {
 							'cachevalue' => serialize(array('dateline' => $_G['timestamp'])),
 							'dateline' => $_G['timestamp'],
 						), false, true);
-						$deletetips = $_G['config']['admincp']['dbimport'] ? cplang('db_delete_tips', array('filename' => basename($zipfilename), 'FORMHASH' => formhash())) : '';
+						$deletetips = $_G['config']['admincp']['dbimport'] ? cplang('db_delete_tips', array('filename' => basename($backupfilename), 'FORMHASH' => formhash())) : '';
 						cpmsg('database_export_multivol_succeed', '', 'succeed', array('volume' => $volume, 'filelist' => $filelist, 'deletetips' => $deletetips));
 					}
 					unset($sqldump, $zip, $content);
